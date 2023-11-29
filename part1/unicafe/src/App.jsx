b2235ead5c8c9668e1605f13605048c88b7c5858
YBCS <budha627@gmail.com>
2023-11-30 01:56:57 +0530

    1.10 unicafe step5

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,33 @@
 import { useState } from "react";
 
+// a proper place to define a component
+const Statistics = ({ good, neutral, bad, all, positive }) => {
+  return (
+    <div>
+      {/* there should be empty line here */}
+      <StatisticLine stat_name="good" stat_value={good} />
+      <StatisticLine stat_name="neutral" stat_value={neutral} />
+      <StatisticLine stat_name="bad" stat_value={bad} />
+      <StatisticLine stat_name="all" stat_value={all} />
+      <StatisticLine stat_name="average" stat_value={all / 3} />{" "}
+      {/* idk what kinda formula is used here */}
+      <StatisticLine
+        stat_name="positive"
+        stat_value={positive}
+        stat_symbol="%"
+      />
+    </div>
+  );
+};
+
+const StatisticLine = ({ stat_name, stat_value, stat_symbol = "" }) => {
+  return (
+    <div>
+      {stat_name} {" " + stat_value + " " + stat_symbol}{" "}
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -8,6 +36,7 @@ const App = () => {
 
   const all = good + neutral + bad;
   const positive = (good / all) * 100;
+  const isFeedback = good || neutral || bad;
   return (
     <div>
       <h1>give feedback</h1>
@@ -18,13 +47,19 @@ const App = () => {
       </div>
       <h1>statistics</h1>
       <div>
-        {/* there should be empty line here */}
-        <div>good {" " + good}</div>
-        <div>neutral {" " + neutral}</div>
-        <div>bad {" " + bad}</div>
-        <div>all {" " + all}</div>
-        <div>average {" " + all / 3}</div> {/* idk what kinda formula is used here */}
-        <div>positive {" " + positive + " %"}</div>
+        {isFeedback ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            all={all}
+            positive={positive}
+          />
+        ) : (
+          <div>
+            <p>No feedback given</p>
+          </div>
+        )}
       </div>
     </div>
   );
