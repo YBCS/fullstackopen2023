4182a2f497e8f543f7fc9da11bf7c0066fef796a
YBCS <budha627@gmail.com>
2023-12-01 01:52:46 +0530

    2.10: The Phonebook Step5

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,43 +1,106 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 }, // dummy values for testing purposes
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filterName, setFilterName] = useState("");
 
   const onSubmit = (event) => {
-	event.preventDefault();
-	const checkName = newName.trim();
-	// TODO : do I need to handle lower case ?
-	const found = persons.find((person) => person.name === checkName);
-	if (found) {
-		alert(`${newName} is already added to phonebook`);
-		return;
-	}
-	const newPersons = persons.concat({name: checkName});
-	setPersons(newPersons);
-  }
+    event.preventDefault();
+    const checkName = newName.trim();
+    const checkNumber = newNumber.trim();
+    // TODO : do I need to handle lower case ?
+    const foundPerson = persons.find((person) => person.name === checkName);
+    const foundNumber = persons.find((person) => person.number === checkNumber);
 
-  const onChange = (event) => { 
-	setNewName(event.target.value);
-  }
+    if (foundPerson) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    if (foundNumber) {
+      alert(`${newNumber} is already added to phonebook`);
+      return;
+    }
+
+    const newPersons = persons.concat({ name: checkName, number: checkNumber });
+    setPersons(newPersons);
+    setNewName("");
+    setNewNumber("");
+    setFilterName("");
+  };
+
+  const onChange = (event) => {
+    setNewName(event.target.value);
+  };
+  const onChangeNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
+  const onChangeFilter = (event) => {
+    setFilterName(event.target.value);
+  };
+
+  const personsToShow = filterName
+    ? persons.filter((person) =>
+        person.name.toLowerCase().includes(filterName.toLowerCase())
+      )
+    : persons;
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input value={newName} onChange={onChange}/>
-        </div>
-        <div>
-          <button type="submit" onClick={onSubmit}>add</button>
-        </div>
-      </form>
+      <Filter filterName={filterName} onChangeFilter={onChangeFilter} />
+      <h2>Add a new</h2>
+      <PersonForm
+        newName={newName}
+        onChange={onChange}
+        newNumber={newNumber}
+        onChangeNumber={onChangeNumber}
+        onSubmit={onSubmit}
+      />
       <h2>Numbers</h2>
-      {
-		persons.map((person) => (<div key={person.name}>{person.name}</div>))
-	  }
+      <Persons persons={personsToShow} />
     </div>
   );
 };
+
+const Filter = ({ filterName, onChangeFilter }) => (
+  <div>
+    {/* how da hello does chat gpt knows exaclty as it is in material; can it see my browser ? */}
+    filter shown with <input value={filterName} onChange={onChangeFilter} />
+  </div>
+);
+
+const PersonForm = ({
+  newName,
+  onChange,
+  newNumber,
+  onChangeNumber,
+  onSubmit,
+}) => (
+  <form>
+    <div>
+      name: <input value={newName} onChange={onChange} />
+    </div>
+    <div>
+      number: <input value={newNumber} onChange={onChangeNumber} />
+    </div>
+    <div>
+      <button type="submit" onClick={onSubmit}>
+        add
+      </button>
+    </div>
+  </form>
+);
+
+const Persons = ({ persons }) =>
+  persons.map((person) => (
+    <div key={person.name}>{`${person.name} ${person.number}`}</div>
+  ));
 
 export default App;
