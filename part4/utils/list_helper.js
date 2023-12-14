ee6e0b0bc6c1ba5e3259568384416fb18c17c89e
YBCS <budha627@gmail.com>
2023-12-14 16:47:34 +0530

    4.7*: helper functions and unit tests, step5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,43 @@
+const _ = require("lodash");
+
 const dummy = (blogs) => {
-  return 1
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes;
+  };
+  return blogs.reduce(reducer, 0);
+};
+
+const mostLikes = (blogs) => {
+  let blog = _.sortBy(blogs, "likes").reverse()[0];
+  return { title: blog.title, author: blog.author, likes: blog.likes };
+};
+
+// check the solution on this one
+const mostBlogs = (blogs) => {
+  let byAuthor = _.groupBy(blogs, "author")
+  let out = null
+  let mostBlogs = 0;
+  for (const author in byAuthor) {
+    const count = byAuthor[author].length
+    if (count > mostBlogs) {
+        out = {
+            author: author,
+            blogs: count
+        }
+        mostBlogs = count
+    }
+  }  
+
+  return out;
 };
 
 module.exports = {
   dummy,
+  totalLikes,
+  mostLikes,
+  mostBlogs,
 };
