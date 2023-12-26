249312b572c1826eb2b6e00fb4b9f14ab600b9b5
YBCS <budha627@gmail.com>
2023-12-27 01:45:31 +0530

    6.9 Better anecdotes, step7; filter redux

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,19 +1,29 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { castVoteOf } from '../reducers/anecdoteReducer'
+import Filter from './Filter'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state)
+  const anecdotes = useSelector((state) => state.anecdotes)
+  const filterName = useSelector((state) => state.filter) // redux is very composable
   const dispatch = useDispatch()
 
   const vote = (id) => {
-    console.log('vote', id)
     dispatch(castVoteOf(id))
   }
 
-  const anecdotesSortedByVotes = anecdotes.sort((a, b) => b.votes - a.votes)
+  const anecdotesToShow = filterName
+    ? anecdotes.filter((c) =>
+        c.content.toLowerCase().includes(filterName.toLowerCase())
+      )
+    : anecdotes
+
+  const anecdotesSortedByVotes = anecdotesToShow.sort(
+    (a, b) => b.votes - a.votes
+  )
 
   return (
     <>
+      <Filter />
       {anecdotesSortedByVotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,14 +1,20 @@
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
+import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import anecdoteReducer from './reducers/anecdoteReducer'
+import filterReducer from './reducers/filterReducer'
+
+const reducer = combineReducers({
+  anecdotes: anecdoteReducer,
+  filter: filterReducer
+})
 
 const store = createStore(reducer)
 
 store.subscribe(() => {
   const storeNow = store.getState()
-  console.log('store now ',storeNow)
+  console.log('store now in subscribe ', storeNow)
 })
 
 
