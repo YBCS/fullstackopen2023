659bc81df883d711cb1e91eade3f7a42ef2ccb7b
YBCS <budha627@gmail.com>
2024-01-24 01:41:30 +0530

    Exercise 6.23 useContext, useReducer

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -2,19 +2,30 @@ import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { getAnecdotes, updateAnecdote } from './requests'
+import { useNotificationDispatch } from './NotificationContext'
 
 const App = () => {
   const queryClient = useQueryClient()
-  
+  const dispatch = useNotificationDispatch()
+
   const voteAnecdoteMutation = useMutation({
     mutationFn: updateAnecdote, // what if I want to use the return value of the mutationFn? --> onSuccess
-    onSuccess: (newAnecdote) => {
+    onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['anecdotes'] }) // this makes sure that the query is refetched when the mutation is completed
     },
   })
 
   const handleVote = (anecdote) => {
     voteAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
+    dispatch({
+      type: 'SET',
+      notification: `anecdote ${anecdote.content} voted`,
+    })
+
+    // todo : that timeout bug still exist ; I might have fixed it in my 2020 code
+    setTimeout(() => {
+      dispatch({ type: 'CLEAR' })
+    }, 3000)
     console.log('vote')
   }
 
--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,15 +1,28 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { createAnecdote } from '../requests'
+import { useNotificationDispatch } from '../NotificationContext'
 
 const AnecdoteForm = () => {
   const queryClient = useQueryClient()
+  const dispatch = useNotificationDispatch()
+
   const newAnecdoteMutation = useMutation({
     mutationFn: createAnecdote,
     onSuccess: (newAnecdote) => {
       console.log('on success new anecdote called 1 ', newAnecdote)
 
       queryClient.invalidateQueries({ queryKey: ['anecdotes'] }) // this makes sure that the query is refetched when the mutation is completed
-      
+      dispatch({
+        type: 'SET',
+        notification: `new anecdote ${newAnecdote.content} created`,
+      })
+
+      setTimeout(() => {
+        dispatch({
+          type: 'CLEAR',
+        })
+      }, 3000)
+
       // manually updating state in react query
       // const anecdotes = queryClient.getQueryData({ queryKey: ['anecdotes'] }) // todo : this is not working
       // // console.log('on success new anecdote called ', anecdotes)
