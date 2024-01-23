613530f9434cdbd7f62f784e352c52bc16c05a8e
YBCS <budha627@gmail.com>
2024-01-24 00:50:04 +0530

    Exercise 6.22 voting

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,48 +1,57 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './requests'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from './requests'
 
 const App = () => {
+  const queryClient = useQueryClient()
+  
+  const voteAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote, // what if I want to use the return value of the mutationFn? --> onSuccess
+    onSuccess: (newAnecdote) => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] }) // this makes sure that the query is refetched when the mutation is completed
+    },
+  })
 
   const handleVote = (anecdote) => {
+    voteAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
     console.log('vote')
   }
 
   const result = useQuery({
     queryKey: ['anecdotes'],
     queryFn: getAnecdotes,
-    retry: 1
+    retry: 1,
   })
   console.log(JSON.parse(JSON.stringify(result)))
 
   const anecdotes = result.data
-  
-  if ( result.isLoading ) {
+
+  if (result.isLoading) {
     return <div>loading data...</div>
   }
-  if ( result.isError ) {
-    return <div>anecdote service not available due to problems in the server</div>
+  if (result.isError) {
+    return (
+      <div>anecdote service not available due to problems in the server</div>
+    )
   }
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
+
       <Notification />
       <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
+
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
     </div>
   )
 }
--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,17 +1,38 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests'
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (newAnecdote) => {
+      console.log('on success new anecdote called 1 ', newAnecdote)
+
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] }) // this makes sure that the query is refetched when the mutation is completed
+      
+      // manually updating state in react query
+      // const anecdotes = queryClient.getQueryData({ queryKey: ['anecdotes'] }) // todo : this is not working
+      // // console.log('on success new anecdote called ', anecdotes)
+      // queryClient.setQueryData(
+      //   { queryKey: ['anecdotes'] },
+      //   anecdotes.concat(newAnecdote)
+      // )
+    },
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
+    newAnecdoteMutation.mutate({ content, votes: 0 })
     console.log('new anecdote')
-}
+  }
 
   return (
     <div>
       <h3>create new</h3>
       <form onSubmit={onCreate}>
-        <input name='anecdote' />
+        <input name="anecdote" />
         <button type="submit">create</button>
       </form>
     </div>
